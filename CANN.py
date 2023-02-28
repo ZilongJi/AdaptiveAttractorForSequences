fcfcbf04d92278dd14c9e21c88f677d689f2af82
ZIlong Ji <jzlxmu@163.com>
2023-02-28 15:23:08 +0800

    add CANN python class

---
+++ CANN.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 28 15:18:47 2023
+
+This code describes the continuous attractor class,
+including 1-dimensional CANN and 2-dimensional CANN
+
+@author: Zilong
+"""
+
