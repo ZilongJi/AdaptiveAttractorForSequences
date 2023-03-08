a655a840cbe8c955fcfc36b6be474cc47b7abf38
Tianhao Chu <chutianhao@stu.pku.edu.cn>
2023-03-08 16:19:12 +0800

    update

--- Theta_sweeps.py
+++ Theta_sweeps.py
@@ -5,9 +5,10 @@ import jax
 import seaborn as sns
 import matplotlib.pyplot as plt
 from cann_fft import CANN1D
+import scipy
 bm.set_platform('cpu')
 
-cann = CANN1D(tau=3, tau_v=144., num=128, mbar=155)
+cann = CANN1D(tau=3, tau_v=144., num=128, mbar=150.5)
 v_ext = cann.a / cann.tau_v * 0.55
 dur =  2.5*np.pi / v_ext
 dt = bm.get_dt()
@@ -26,24 +27,42 @@ Iext = cann.get_stimulus_by_pos(position)
 
 runner = bp.DSRunner(cann,
                      inputs=('input', Iext, 'iter'),
-                     monitors=['u', 'v', 'r','centerI'])
+                     monitors=['u', 'v', 'r','center','centerI'])
 
 runner.run(dur)
-probe_num = int( 2*bm.pi / v_ext/dt)
+probe_num = int( 1.9*bm.pi / v_ext/dt)
+time=time[probe_num:-1]
+##Visualize theta sweeps
 plt.figure()
 index = np.linspace(1, cann.num, cann.num)
 pos = np.linspace(-np.pi,np.pi,cann.num+1)
 pos = pos[0:-1]
 fr = runner.mon.r.T[:,probe_num:-1]
-plt.pcolormesh(time[probe_num:-1], pos, fr,cmap='jet')
+plt.pcolormesh(time, pos, fr, cmap='jet')
 cI = runner.mon.centerI[probe_num:-1]
-
-plt.plot(time[probe_num:-1], cI, color='k')
+plt.plot(time, cI, color='k')
 plt.xlabel('time(ms)', fontsize=15)
 plt.ylabel('Encoded Position', fontsize=15)
 plt.ylim([-2.5, 2.5])
 plt.colorbar()
 plt.show()
+##Calculate theta phase
+relative_pos = runner.mon.center[probe_num:-1] - runner.mon.centerI[probe_num:-1]
+relative_pos = bm.where(relative_pos > np.pi, relative_pos - 2*np.pi,  relative_pos)
+relative_pos = bm.where(relative_pos < -np.pi, relative_pos + 2*np.pi,  relative_pos)
+relative_pos = np.squeeze(relative_pos)
+Peaks,_ = scipy.signal.find_peaks(relative_pos, width=100)
+Period = np.mean(np.diff(time[Peaks]))
+phase = np.zeros(len(time))
+for ti in range(len(time)):
+    time_residual = time[ti]-time[Peaks]
+    if np.max(time_residual)>0:
+        time_residual[time_residual < 0] = 2*Period
+        phase[ti] = np.min(time_residual)/Period*2*np.pi
+    else:
+        phase[ti] = (Period+np.max(time_residual)) / Period * 2 * np.pi
+
+
 
 
 
