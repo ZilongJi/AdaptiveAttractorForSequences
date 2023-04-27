73ed8f3000142da395b870a8a3d03609fd894f55
Tianhao Chu <chutianhao@stu.pku.edu.cn>
2023-04-27 22:03:42 +0800

    fig 5d

--- fig5e.py
+++ fig5e.py
@@ -15,20 +15,19 @@ cx = center_trace[w_start:(w_start + w_size), 0]
 cy = center_trace[w_start:(w_start + w_size), 1]
 st = step[w_start:(w_start + w_size)]
 mr = mean_fr[w_start:(w_start + w_size)]
-period = int(20)
-phase_step = np.zeros(period)
-phase_r = np.zeros(period)
+period = 20
+phase_step = np.zeros(1, period)
+phase_r = np.zeros(1, period)
 phase = np.linspace(-np.pi, np.pi, period)
-for i in range(int(len(mean_fr) / period)):
+for i in range(np.floor(len(mean_fr) / period)):
     for j in range(period):
-        phase_step[j] = phase_step[j] + step[i * period + j]
-        phase_r[j] = phase_r[j] + mean_fr[i * period + j]
+        phase_step[j] = phase_step[j] + step[i * period + j + 5]
+        phase_r[j] = phase_r[j] + mean_fr[i * period + j + 5]
 
 fig1, axs = plt.subplots(2, 1, figsize=(4, 6), sharex=True)
 axs[0].plot(phase, phase_r)
-down_sample = 1
-axs[0].bar(phase[0:-1:down_sample], phase_r[0:-1:down_sample], width=np.pi/period)
-axs[1].plot(phase[0:-1], phase_step[0:-1])
-axs[1].bar(phase[0:-1:down_sample], phase_step[0:-1:down_sample], width=np.pi/period)
-# axs[0].bar(phase[0:-1:down_sample], phase_step[0:-1:down_sample])
+down_sample = 4
+axs[0].bar(phase[0:-1:down_sample], phase_r[0:-1:down_sample])
+axs[1].plot(phase[0:-1:down_sample], phase_step[0:-1:down_sample])
+axs[0].bar(phase[0:-1:down_sample], phase_step[0:-1:down_sample])
 plt.show()
